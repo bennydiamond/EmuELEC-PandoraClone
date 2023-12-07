b4184dfdcb8d1aa9de5fd9b7358c2dd0d29b215c
bennydiamond <github@benjaminfd.com>
2023-12-07 11:58:18 -0500

    Update a few packages

--- packages/compress/zstd/package.mk
+++ packages/compress/zstd/package.mk
@@ -2,15 +2,23 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="zstd"
-PKG_VERSION="1.4.3"
-PKG_SHA256="e88ec8d420ff228610b77fba4fbf22b9f8b9d3f223a40ef59c9c075fcdad5767"
+PKG_VERSION="1.5.2"
+PKG_SHA256="7c42d56fac126929a6a85dbc73ff1db2411d04f104fae9bdea51305663a83fd0"
 PKG_LICENSE="BSD/GPLv2"
 PKG_SITE="http://www.zstd.net"
 PKG_URL="https://github.com/facebook/zstd/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_HOST="gcc:host ninja:host"
+PKG_DEPENDS_HOST="ccache:host meson:host ninja:host"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A fast real-time compression algorithm."
+PKG_TOOLCHAIN="meson"
 
 configure_package() {
-  PKG_CMAKE_SCRIPT="${PKG_BUILD}/build/cmake/CMakeLists.txt"
+  PKG_MESON_SCRIPT="${PKG_BUILD}/build/meson/meson.build"
 }
+
+PKG_MESON_OPTS_HOST="-Dlegacy_level=0 \
+                     -Dbin_programs=false \
+                     -Dzlib=disabled \
+                     -Dlzma=disabled \
+                     -Dlz4=disabled"
+
